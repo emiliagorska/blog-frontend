53e7d881f1abc5d952a212a008be583f5f9e4b0b
Emilia <emilia.gorska@and.digital>
2022-10-18 09:37:15 +0100

    added truncation of post content inn blogCard compomnent using javascipt

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -2,13 +2,17 @@ import React from "react";
 import "./BlogCard.css";
 
 function BlogCard({ post }) {
+  //124 characters in one line
+  var postContentwithoutEllipsis = post.content.slice(0, 480);
+  var postContent = postContentwithoutEllipsis.concat("", "...");
+
   return (
     <div className="post">
       <img className="postImage" src={post.image} alt={post.title}></img>
       <a href="/" className="postTitle">
         {post.title}
       </a>
-      <article className="postContent">{post.content}</article>
+      <article className="postContent">{postContent}</article>
       <p> &#x1F4C5; {post.createdDate}</p>
     </div>
   );
