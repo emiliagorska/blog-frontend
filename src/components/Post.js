86d773e4ebb7f5937be8482641f6a9aec07cc494
Emilia Gorska <emilia.gorska@and.digital>
2023-01-12 14:45:01 +0000

    Outlined structure of the page in a comment

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,7 +4,19 @@ import { useParams } from "react-router-dom";
 function Post({ postid }) {
   let params = useParams();
 
-  return <div>This is a single post page for post with id {params.id}</div>;
+  return (
+    <>
+      <div>This is a single post page for post with id {params.id}</div>
+      <div>
+        <a href="/">Edinburgh Guide</a>
+      </div>
+    </>
+  );
+  // Header - with name of the blog on the left and expandable bar on the right
+  // Image
+  // Title
+  // Content
+  //Footer - perhaps with 'leave a comment' section
 }
 
 export default Post;
